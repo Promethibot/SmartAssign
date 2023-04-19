248193ed8783164d0267cbf210422b940e7da8e4
Promethibot <paola.mancini@me.com>
2023-04-18 23:03:18 -0400

    Add project files.

---
+++ SmartAssign/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblMonth = New System.Windows.Forms.Label()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.cboMonths = New System.Windows.Forms.ComboBox()
+        Me.lblAvg = New System.Windows.Forms.Label()
+        Me.lblSignificant = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, -9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(439, 325)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Arial Black", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(457, 45)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(331, 28)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Smart Home Electric Savings"
+        '
+        'lblMonth
+        '
+        Me.lblMonth.AutoSize = True
+        Me.lblMonth.Font = New System.Drawing.Font("Arial Black", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMonth.Location = New System.Drawing.Point(213, 325)
+        Me.lblMonth.Name = "lblMonth"
+        Me.lblMonth.Size = New System.Drawing.Size(224, 23)
+        Me.lblMonth.TabIndex = 2
+        Me.lblMonth.Text = "The electric savings for "
+        Me.lblMonth.Visible = False
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnDisplay.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplay.Location = New System.Drawing.Point(319, 357)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(160, 30)
+        Me.btnDisplay.TabIndex = 3
+        Me.btnDisplay.Text = "Display Statistics"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        Me.btnDisplay.Visible = False
+        '
+        'cboMonths
+        '
+        Me.cboMonths.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboMonths.FormattingEnabled = True
+        Me.cboMonths.Location = New System.Drawing.Point(492, 111)
+        Me.cboMonths.Name = "cboMonths"
+        Me.cboMonths.Size = New System.Drawing.Size(250, 27)
+        Me.cboMonths.TabIndex = 4
+        Me.cboMonths.Text = "Please select a month"
+        '
+        'lblAvg
+        '
+        Me.lblAvg.AutoSize = True
+        Me.lblAvg.Font = New System.Drawing.Font("Arial Black", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAvg.Location = New System.Drawing.Point(230, 399)
+        Me.lblAvg.Name = "lblAvg"
+        Me.lblAvg.Size = New System.Drawing.Size(304, 23)
+        Me.lblAvg.TabIndex = 5
+        Me.lblAvg.Text = "The average monthly savings are"
+        Me.lblAvg.Visible = False
+        '
+        'lblSignificant
+        '
+        Me.lblSignificant.AutoSize = True
+        Me.lblSignificant.Font = New System.Drawing.Font("Arial Black", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSignificant.Location = New System.Drawing.Point(188, 437)
+        Me.lblSignificant.Name = "lblSignificant"
+        Me.lblSignificant.Size = New System.Drawing.Size(433, 23)
+        Me.lblSignificant.TabIndex = 6
+        Me.lblSignificant.Text = "Month had the most significant monthly savings"
+        Me.lblSignificant.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SeaShell
+        Me.ClientSize = New System.Drawing.Size(800, 486)
+        Me.Controls.Add(Me.lblSignificant)
+        Me.Controls.Add(Me.lblAvg)
+        Me.Controls.Add(Me.cboMonths)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lblMonth)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Smart Home Application"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblMonth As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents cboMonths As ComboBox
+    Friend WithEvents lblAvg As Label
+    Friend WithEvents lblSignificant As Label
+End Class
---
+++ SmartAssign/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private _intArrSize As Integer = 11
+    Private _strSavings(_intArrSize) As String
+    Private _decBill(_intArrSize) As Decimal
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        lblMonth.Visible = True
+        lblAvg.Visible = True
+        lblSignificant.Visible = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim objReader As IO.StreamReader
+        Dim strSavingsAmount As String
+        Dim intCount As Integer = 0
+        Dim intFill As Integer
+
+
+        Try
+            If IO.File.Exists("D:\OneDrive\Classes\Spring 2023\Graphic User Interface Design\repos\SmartAssign\SmartAssign\savings.txt") Then
+                objReader = IO.File.OpenText("D:\OneDrive\Classes\Spring 2023\Graphic User Interface Design\repos\SmartAssign\SmartAssign\savings.txt")
+                Do While objReader.Peek <> -1
+                    _strSavings(intCount) = objReader.ReadLine
+                    strSavingsAmount = objReader.ReadLine
+                    _decBill(intCount) = Convert.ToDecimal(strSavingsAmount)
+                    intCount += 1
+                Loop
+
+                objReader.Close()
+
+                For intFill = 0 To (_strSavings.Length - 1)
+                    cboMonths.Items.Add(_strSavings(intFill))
+                Next
+
+            Else
+                MsgBox("The file could not be found, sorry!", , "Error")
+            End If
+
+        Catch Exception As IOException
+            MsgBox("Something went wrong with the IO, sorry!")
+        End Try
+
+
+
+    End Sub
+
+    Private Sub cboMonths_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMonths.SelectedIndexChanged
+        Dim intSelectedCityIndex As Integer
+
+        intSelectedCityIndex = cboMonths.SelectedIndex
+
+        lblMonth.Text = "The electric savings for " + _strSavings(intSelectedCityIndex) + " is $" + _decBill(intSelectedCityIndex).ToString
+        btnDisplay.Visible = True
+
+        ComputeAverage()
+        ComputeMaxSavings()
+
+
+    End Sub
+
+    Private Sub ComputeMaxSavings()
+        Dim intMonths As Integer
+        Dim intLargestSavingsValue As Integer = 0
+        Dim intIndexValue As Integer = 0
+        Dim strYearValue As String = ""
+
+        For Each intMonths In _decBill
+            intLargestSavingsValue = Math.Max(intLargestSavingsValue, intMonths)
+            If (intMonths >= intLargestSavingsValue) Then
+                strYearValue = _strSavings(intIndexValue)
+            End If
+            intIndexValue += 1
+        Next
+
+        lblSignificant.Text = strYearValue + " had the most significant monthly savings"
+
+    End Sub
+
+    Private Sub ComputeAverage()
+        Dim intCountYears As Integer
+        Dim intYears As Integer = 0
+        Dim decTotalBill As Decimal = 0
+        Dim decAverageNumberOfBill As Decimal = 0D
+
+        For Each intCountYears In _decBill
+            decTotalBill += _decBill(intYears)
+            intYears += 1
+        Next
+
+        decAverageNumberOfBill = decTotalBill / Convert.ToDecimal(_decBill.Length())
+
+        lblAvg.Text = "The average monthly savings are " + decAverageNumberOfBill.ToString("C2")
+
+    End Sub
+End Class
